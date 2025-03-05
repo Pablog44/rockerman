c2dbc92c791ec2a1494072cdf3ca87c1204dc274
Pablog44 <PABLOGONZALEZCUADRADO@HOTMAIL.COM>
2025-03-05 02:39:06 +0100

    comenzamos.
    Empieza el proyecto de este videojuego con inspiración en bomberman.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import PhaserGame from './PhaserGame';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Juego tipo Bomberman</h1>
+      <PhaserGame />
+    </div>
+  );
+}
+
+export default App;
